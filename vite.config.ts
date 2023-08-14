9c7f648b969264590e7a14bcb3415f30dd4f6ab3
Nikita Podgornyi <nikpdg13@gmail.com>
2023-08-14 18:58:46 +0300

    refactor: base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
-    if (command === 'serve') {
-        return {
-            plugins: [react()]
-        }
-    } else {
-        return {
-            plugins: [react()],
-            base: '/react-todo/'
-        }
+    return {
+        plugins: [react()],
+        base: command === 'serve' ? '/react-todo/' : '/'
     }
 })
