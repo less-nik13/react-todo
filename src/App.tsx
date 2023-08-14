5c67a5b217494d609e87733ddc8ccfa503318c3e
Nikita Podgornyi <nikpdg13@gmail.com>
2023-08-14 18:11:01 +0300

    feat: added page with dinamic path

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import HomePage from './pages/HomePage/HomePage'
 import AboutPage from './pages/AboutPage/AboutPage'
+import TestPage from './pages/TestPage/TestPage'
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
 
 import './App.css'
@@ -16,6 +17,7 @@ function App() {
             <Routes>
                 <Route index element={<HomePage />}></Route>
                 <Route path='/about' element={<AboutPage />}></Route>
+                <Route path='/test/:id' element={<TestPage />}></Route>
                 <Route path='*' element={<NotFoundPage />}></Route>
             </Routes>
         </BrowserRouter>
